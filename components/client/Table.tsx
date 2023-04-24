002e5e11156d7fd7ff8c8891e1cf599ee6286a78
dweinber <daniel.weinberger@bitmovin.com>
2023-04-24 20:36:08 +0200

    Use type === 'date' to format numbers as local date time string

--- components/client/Table.tsx
+++ components/client/Table.tsx
@@ -4,6 +4,7 @@ import React, { Suspense, lazy } from "react";
 
 import type { TableProps } from "@cloudscape-design/components/table";
 import Spinner from "./Spinner";
+import { intlFormat } from "date-fns";
 
 const LazyTable = lazy(() => import("@cloudscape-design/components/table"));
 
@@ -13,7 +14,7 @@ if (typeof window === "undefined") {
 
 export default function Table(
   props: Omit<TableProps<unknown>, "trackBy" | "isItemDisabled" | "columnDefinitions"> & {
-    columns: { id: string; children?: JSX.Element | string }[];
+    columns: { id: string; children?: JSX.Element | string; type?: 'date' }[];
   }
 ) {
   return (
@@ -32,6 +33,17 @@ export default function Table(
             header: column.children ? <>{column.children}</> : <></>,
             ariaLabel: (data) => `${data}${column}`,
             cell: <T extends { [x: string]: JSX.Element }>(item: T) => {
+              if (column.type === 'date') {
+                console.log(`formatting ${item[column.id]} as date...`);
+                return intlFormat(new Date(item[column.id] as any), {
+                  year: "2-digit",
+                  month: "2-digit",
+                  day: "2-digit",
+                  weekday: "short",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                });
+              }
               return item[column.id];
             },
           })) || []) as TableProps<unknown>["columnDefinitions"]
