12350ccd09ef93a06f4163a0f25cdbb8f231063f
dweinber <daniel.weinberger@bitmovin.com>
2023-05-09 11:19:38 +0200

    Create a better overview of session details

--- components/UserSession.tsx
+++ components/UserSession.tsx
@@ -37,64 +37,247 @@ async function Component(props: UserSessionProps) {
     // Example: Types define `browserVersionMajor` but API returns `browser_version_major`
     const firstSample = (analyticsSamples as any)[0];
 
-    const customDataLiArray = [];
-    for (let i = 1; i <= 30; i++) {
-      customDataLiArray.push(
-        <li>
-          Custom Data {i}: {firstSample[`custom_data_${i}`]}
-        </li>
+    const customDataFields = [];
+    for (let i = 2; i <= 30; i += 3) {
+      customDataFields.push(
+        <tr>
+          {firstSample[`custom_data_${i}`] ? <td>Custom Data {i}</td> : <td></td>}
+          {firstSample[`custom_data_${i}`] ? (
+            <td>
+              <b>{firstSample[`custom_data_${i}`]}</b>
+            </td>
+          ) : (
+            <td></td>
+          )}
+          {firstSample[`custom_data_${i + 1}`] ? <td>Custom Data {i + 1}</td> : <td></td>}
+          {firstSample[`custom_data_${i + 1}`] ? (
+            <td>
+              <b>{firstSample[`custom_data_${i + 1}`]}</b>
+            </td>
+          ) : (
+            <td></td>
+          )}
+          {firstSample[`custom_data_${i + 2}`] ? <td>Custom Data {i + 2}</td> : <td></td>}
+          {firstSample[`custom_data_${i + 2}`] ? (
+            <td>
+              <b>{firstSample[`custom_data_${i + 2}`]}</b>
+            </td>
+          ) : (
+            <td></td>
+          )}
+        </tr>
       );
     }
 
     const errorSample = (analyticsSamples as any).find((sample: any) => sample.state === "error");
     const errorInfoElement = errorSample ? (
-      <li>
-        Error: {errorSample.error_message} ({errorSample.error_code}). Details: {errorSample.data}
-      </li>
+      <tr>
+        <td>Error Code</td>
+        <td>
+          <b>{errorSample.error_code ?? "No code available"}</b>
+        </td>
+        <td>Error Message</td>
+        <td>
+          <b>{errorSample.error_message ?? "No message available"}</b>
+        </td>
+        <td>Additional Data</td>
+        <td>
+          <b>{errorSample ? JSON.stringify(errorSample.data) : "No additional data available"}</b>
+        </td>
+      </tr>
     ) : (
-      <li>No error occurred</li>
+      <tr>
+        <td colSpan={2}>
+          <b>No error occurred</b>
+        </td>
+      </tr>
     );
 
     return (
-      <ul>
-        <li>
-          Analytics Version: {firstSample.platform} {firstSample.analytics_version}
-        </li>
-        {errorInfoElement}
-        <li>
-          Browser: {firstSample.browser} {firstSample.browser_version_major}.{firstSample.browser_version_minor}
-        </li>
-        <li>
-          Device Type/Class: {firstSample.device_type}/{firstSample.device_class}
-        </li>
-        <li>
-          Operating System: {firstSample.operatingsystem} {firstSample.operatingsystem_version_major}.
-          {firstSample.operatingsystem_version_minor}
-        </li>
-        <li>
-          Location (IP Address): {firstSample.city.charAt(0).toUpperCase() + firstSample.city.slice(1)},{" "}
-          {firstSample.country} ({firstSample.ip_address})
-        </li>
-        <li>
-          User ID / Custom User ID: {firstSample.user_id} / {firstSample.custom_user_id}
-        </li>
-        <li>
-          ISP / CDN: {firstSample.isp} / {firstSample.cdn_provider}
-        </li>
-        <li>
-          URL: {firstSample.domain}
-          {firstSample.path}
-        </li>
-        <li>
-          Video Title / ID: {firstSample.video_title} / {firstSample.video_id}
-        </li>
-        <li>Player Version: {firstSample.player_version}</li>
-        <li>
-          Screen Size: {firstSample.screen_width}x{firstSample.screen_height}
-        </li>
-        {customDataLiArray}
-        <li>Experiment Name: {firstSample.experiment_name}</li>
-      </ul>
+      <>
+        <table width="100%">
+          <thead>
+            <tr>
+              <th colSpan={6}>Errors</th>
+            </tr>
+          </thead>
+          <tbody>
+            {errorInfoElement}
+          </tbody>
+          <thead>
+            <tr>
+              <th colSpan={6}>Device</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Operating System</td>
+              <td>
+                <b>
+                  {firstSample.operatingsystem} {firstSample.operatingsystem_version_major}.
+                  {firstSample.operatingsystem_version_minor}
+                </b>
+              </td>
+              <td>ISP</td>
+              <td>
+                <b>{firstSample.isp}</b>
+              </td>
+              <td>User ID</td>
+              <td>
+                <b>{firstSample.user_id}</b>
+              </td>
+            </tr>
+            <tr>
+              <td>Browser</td>
+              <td>
+                <b>
+                  {firstSample.browser} {firstSample.browser_version_major}.{firstSample.browser_version_minor}
+                </b>
+              </td>
+              <td>IP Address</td>
+              <td>
+                <b>{firstSample.ip_address}</b>
+              </td>
+              <td>Custom User ID</td>
+              <td>
+                <b>{firstSample.custom_user_id}</b>
+              </td>
+            </tr>
+            <tr>
+              <td>Device Type</td>
+              <td>
+                <b>
+                  {firstSample.device_type} ({firstSample.device_class})
+                </b>
+              </td>
+              <td>Screen Size</td>
+              <td>
+                <b>
+                  {firstSample.screen_width}x{firstSample.screen_height}
+                </b>
+              </td>
+              <td>Visited Page</td>
+              <td>
+                <b>
+                  {firstSample.domain}
+                  {firstSample.path}
+                </b>
+              </td>
+            </tr>
+          </tbody>
+          <thead>
+            <tr>
+              <th colSpan={6}>Player</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Player Technology</td>
+              <td></td>
+              <td>Analytics Version</td>
+              <td>
+                <b>
+                  {firstSample.platform} {firstSample.analytics_version}
+                </b>
+              </td>
+              <td>Session Type</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Player Version</td>
+              <td>
+                <b>{firstSample.player_version}</b>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+          <thead>
+            <tr>
+              <th colSpan={6}>Stream</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Stream Format</td>
+              <td></td>
+              <td>Video ID</td>
+              <td>
+                <b>{firstSample.video_id}</b>
+              </td>
+              <td>Audio Language</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Audio Codec</td>
+              <td></td>
+              <td>Video Title</td>
+              <td>
+                <b>{firstSample.video_title}</b>
+              </td>
+              <td>Subtitles</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Video Codec</td>
+              <td></td>
+              <td>Video Duration</td>
+              <td></td>
+              <td>CDN Provider</td>
+              <td>
+                <b>{firstSample.cdn_provider}</b>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td>Manifest URL</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+          <thead>
+            <tr>
+              <th colSpan={6}>Custom Data</th>
+            </tr>
+          </thead>
+          <tbody>
+            {customDataFields}
+            {firstSample.experiment_name ? (
+              <tr>
+                <td>Experiment Name</td>
+                <td>
+                  <b>{firstSample.experiment_name}</b>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            ) : (
+              <></>
+            )}
+          </tbody>
+          <thead>
+            <tr>
+              <th colSpan={6}>User Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>City</td>
+              <td>{firstSample.city.charAt(0).toUpperCase() + firstSample.city.slice(1)}</td>
+              <td>Country</td>
+              <td>{firstSample.country}</td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </>
     );
   } catch (e) {
     const safeError = z.instanceof(Error).parse(e);
