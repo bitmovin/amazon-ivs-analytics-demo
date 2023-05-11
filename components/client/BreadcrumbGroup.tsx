7a3e43cc2c523385746be295423a08303150836f
dweinber <daniel.weinberger@bitmovin.com>
2023-05-11 17:15:32 +0200

    Automatically create breadcrumbs

--- components/client/BreadcrumbGroup.tsx
+++ components/client/BreadcrumbGroup.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import type { BreadcrumbGroupProps } from "@cloudscape-design/components/breadcrumb-group";
 import { Route } from "next";
 import { useRouter } from "next/navigation";
 import React, { Suspense, lazy } from "react";
+import { useRoute } from "./hooks/useQuery";
 
 const LazyBreadcrumbGroup = lazy(() => import("@cloudscape-design/components/breadcrumb-group"));
 
@@ -11,19 +11,23 @@ if (typeof window === "undefined") {
   React.useLayoutEffect = () => ({});
 }
 
-export default function ClientBreadcrumbGroup(props: {
-  items: {
-    text: string;
-    href: Route;
-    route: Route;
-  }[];
-}) {
+export default function ClientBreadcrumbGroup(props: {}) {
   const router = useRouter();
+
+  const route = useRoute();
+  const routeParts = route[0].split("?")[0].split("/");
+  const breadcrumbs: { text: string; href: string }[] = [];
+  for (let i = 0; i < routeParts.length; i++) {
+    breadcrumbs.push({
+      text: routeParts[i] || "/",
+      href: (routeParts.slice(0, i + 1).join("/") || "/") + "?" + route[0].split("?")[1],
+    });
+  }
 
   return (
     <Suspense fallback={<span>Loading...</span>}>
       <LazyBreadcrumbGroup
-        items={props.items}
+        items={breadcrumbs}
         onFollow={(e) => {
           e.preventDefault();
           router.replace(e.detail.href as Route);
